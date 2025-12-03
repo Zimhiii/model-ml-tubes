c0a26b5e42e832f2950d4fa98e037b25cbe7e3b4
Zimhiii <hijazmih@gmail.com>
2025-12-04 00:55:28 +0700

    fix

--- main.py
+++ main.py
@@ -137,5 +137,5 @@ async def predict(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
